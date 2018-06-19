983a1d46b43310dc7a68c55330b275eddba0e837
Zenkin <zenkinartem1997@gmail.com>
2018-06-20 02:03:56 +0300

    commited

--- modbus_master_rtu.py
+++ modbus_master_rtu.py
@@ -1,13 +1,40 @@
 #!/usr/bin/env python2.7
+# -*- coding: utf_8 -*-
 
 import serial
-  
-ser = serial.Serial('/dev/ttyUSB0', 9600, bytesize=8, parity='N', stopbits=1, timeout=1)
-  
+
+import modbus_tk
+import modbus_tk.defines as cst
+from modbus_tk import modbus_rtu
+
+PORT = '/dev/ttyUSB0'
+
 def main():
-    while True:
-        s = ser.readline()
-        print(s)
-  
+    logger = modbus_tk.utils.create_logger("console")
+
+    try:
+        #Connect to the slave
+        master = modbus_rtu.RtuMaster(
+            serial.Serial(port=PORT, baudrate=9600, bytesize=8, parity='N', stopbits=1, xonxoff=0)
+        )
+        master.set_timeout(5.0)
+        master.set_verbose(True)
+        logger.info("connected")
+
+        logger.info(master.execute(1, cst.READ_HOLDING_REGISTERS, 0, 3))
+
+        #send some queries
+        #logger.info(master.execute(1, cst.READ_COILS, 0, 10))
+        #logger.info(master.execute(1, cst.READ_DISCRETE_INPUTS, 0, 8))
+        #logger.info(master.execute(1, cst.READ_INPUT_REGISTERS, 100, 3))
+        #logger.info(master.execute(1, cst.READ_HOLDING_REGISTERS, 100, 12))
+        #logger.info(master.execute(1, cst.WRITE_SINGLE_COIL, 7, output_value=1))
+        #logger.info(master.execute(1, cst.WRITE_SINGLE_REGISTER, 100, output_value=54))
+        #logger.info(master.execute(1, cst.WRITE_MULTIPLE_COILS, 0, output_value=[1, 1, 0, 1, 1, 0, 1, 1]))
+        #logger.info(master.execute(1, cst.WRITE_MULTIPLE_REGISTERS, 100, output_value=xrange(12)))
+
+    except modbus_tk.modbus.ModbusError as exc:
+        logger.error("%s- Code=%d", exc, exc.get_exception_code())
+
 if __name__ == "__main__":
     main()
