2f557d89d36c29d68c6b02e8388471c90bc9eec0
Zenkin <zenkinartem1997@gmail.com>
2018-06-20 12:23:39 +0300

    commited

--- modbus_master_rtu.py
+++ modbus_master_rtu.py
@@ -24,7 +24,7 @@ def main():
         #logger.info(master.execute(235, cst.READ_HOLDING_REGISTERS, 0, 3))
 
         #send some queries
-        logger.info(master.execute(16, cst.READ_COILS, 0, 10))
+        logger.info(master.execute(22 cst.READ_COILS, 0, 10))
         #logger.info(master.execute(1, cst.READ_DISCRETE_INPUTS, 0, 8))
         #logger.info(master.execute(1, cst.READ_INPUT_REGISTERS, 100, 3))
         #logger.info(master.execute(1, cst.READ_HOLDING_REGISTERS, 100, 12))
