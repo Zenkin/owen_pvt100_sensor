c2468e148c66dca83b7f1f9c4b04de848b897eaf
Zenkin <zenkinartem1997@gmail.com>
2018-06-20 23:10:58 +0300

    commited

--- ModBus_RTU_master.py
+++ ModBus_RTU_master.py
@@ -8,13 +8,13 @@ slave_adress = 16 # 10cc
 ## Number of the first register 0x0102 16cc or 258 10cc ##
 hex_number = 102
 dec_number = 258
-register_number = dec_number
+register_number = hex_number
 number_of_decimals = 1 # temperature value from -4000 to +12000 C
 
 def main():
     print("starting...")
     instrument = minimalmodbus.Instrument(port, slave_adress)
-    temperature = instrument.read_register(register_number, 1) # Registernumber, number of decimals
-    print(temperature)
+    #temperature = instrument.read_register(register_number, 1) # Registernumber, number of decimals
+    #print(temperature)
 
 main()
