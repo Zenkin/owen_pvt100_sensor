ea48af6fb5634255263dcad14adb021f6e18d36a
Zenkin <zenkinartem1997@gmail.com>
2018-06-21 00:21:33 +0300

    add INFO

--- ModBus_RTU_master.py
+++ ModBus_RTU_master.py
@@ -7,7 +7,7 @@ port = '/dev/ttyUSB0' # serial port
 slave_adress = 16 # 10cc
 ## Number of the first register 0x0102 16cc or 258 10cc ##
 hex_number = 102
-dec_number = 259
+dec_number = 258
 register_number = dec_number
 number_of_decimals = 1 # temperature value from -4000 to +12000 C
 baudrate = 9600 # from datasheet
@@ -19,19 +19,20 @@ def main():
     print("starting...")
 
     instrument = minimalmodbus.Instrument(port, slave_adress)
-    instrument.serial.baudrate = baudrate 
-    instrument.serial.bytesize = bytesize
-    instrument.serial.parity   = serial.PARITY_NONE # from datasheet
-    instrument.serial.timeout  = timeout 
+    instrument.serial.BAUDRATE = baudrate 
+    #instrument.serial.BYTESIZE = bytesize
+    #instrument.serial.PARITY   = serial.PARITY_NONE # from datasheet
+    #instrument.serial.TIMEOUT  = timeout
+    #instrument.serial.STOPBITS = stopbits
 
     print("INFORMATION")
     print("Serial port: " + str(port))
     print("Slave adress: " + str(slave_adress))
-    print("Boudrate: " + str(baudrate))
-    print("Bytesize: " + str(bytesize))
-    print("Stopbits: " + str(stopbits))
-    print("Parity: " + str(serial.PARITY_NONE))
-    print("Timeout: " + str(timeout))
+    print("Boudrate: " + str(instrument.serial.BAUDRATE))
+    print("Bytesize: " + str(instrument.serial.BYTESIZE))
+    print("Stopbits: " + str(instrument.serial.STOPBITS))
+    print("Parity: " + str(instrument.serial.PARITY))
+    print("Timeout: " + str(instrument.serial.TIMEOUT))
 
     temperature = instrument.read_register(register_number, 1) # Registernumber, number of decimals
     print(temperature)
