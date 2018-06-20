8f55c6c004198d334cdad3ea137be2a77cd45bda
Zenkin <zenkinartem1997@gmail.com>
2018-06-20 23:12:52 +0300

    commited

--- ModBus_RTU_master.py
+++ ModBus_RTU_master.py
@@ -4,11 +4,11 @@ import minimalmodbus
 import serial
 
 port = '/dev/ttyUSB0' # serial port
-slave_adress = 0 # 10cc
+slave_adress = 16 # 10cc
 ## Number of the first register 0x0102 16cc or 258 10cc ##
 hex_number = 102
 dec_number = 258
-register_number = hex_number
+register_number = dec_number
 number_of_decimals = 1 # temperature value from -4000 to +12000 C
 
 def main():
