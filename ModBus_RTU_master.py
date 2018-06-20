e26e48cc3f5d542c7e9cce8523b780a882624cf9
Zenkin <zenkinartem1997@gmail.com>
2018-06-20 23:11:59 +0300

    commited

--- ModBus_RTU_master.py
+++ ModBus_RTU_master.py
@@ -4,7 +4,7 @@ import minimalmodbus
 import serial
 
 port = '/dev/ttyUSB0' # serial port
-slave_adress = 16 # 10cc
+slave_adress = 0 # 10cc
 ## Number of the first register 0x0102 16cc or 258 10cc ##
 hex_number = 102
 dec_number = 258
@@ -14,7 +14,7 @@ number_of_decimals = 1 # temperature value from -4000 to +12000 C
 def main():
     print("starting...")
     instrument = minimalmodbus.Instrument(port, slave_adress)
-    #temperature = instrument.read_register(register_number, 1) # Registernumber, number of decimals
-    #print(temperature)
+    temperature = instrument.read_register(register_number, 1) # Registernumber, number of decimals
+    print(temperature)
 
 main()
