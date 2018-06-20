0f9ab658867d6ccaadaef11126382b7ae9918cfd
Zenkin <zenkinartem1997@gmail.com>
2018-06-20 22:09:11 +0300

    only serial

--- new_way.py
+++ new_way.py
@@ -97,9 +97,9 @@ def main():
         parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,
         bytesize=serial.EIGHTBITS)
     for i in xrange(100):
-        y = UA_112(com, 1, 4, 0, 6)
+        y = UA_112(com, 16, 3, 0, 6)
         print y
-        time.sleep(2)
+        time.sleep(0.5)
    
     return 0
 
