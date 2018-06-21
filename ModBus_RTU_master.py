f1c662e3053a94bc8893dd9a1938b542085beba1
Zenkin <zenkinartem1997@gmail.com>
2018-06-21 23:22:48 +0300

    add sensor info

--- ModBus_RTU_master.py
+++ ModBus_RTU_master.py
@@ -18,29 +18,31 @@ class HTT100:
     sensors_count = 0
 
     def __init__(self, port, slave_adress, baudrate, parity, bytesize, stopbits, timeout):
-        minimalmodbus.BAUDRATE = baudrate
-        minimalmodbus.PARITY = parity
-        minimalmodbus.BYTESIZE  = bytesize
-        minimalmodbus.STOPBITS  = stopbits
-        minimalmodbus.TIMEOUT = timeout 
-        self.instrument = minimalmodbus.Instrument(port, slave_adress, mode='rtu')
-        self.instrument.mode = minimalmodbus.MODE_RTU # set rtu mode
+        #minimalmodbus.BAUDRATE = baudrate
+        #minimalmodbus.PARITY = parity
+        #minimalmodbus.BYTESIZE  = bytesize
+        #minimalmodbus.STOPBITS  = stopbits
+        #minimalmodbus.TIMEOUT = timeout 
+        #self.instrument = minimalmodbus.Instrument(port, slave_adress, mode='rtu')
+        #self.instrument.mode = minimalmodbus.MODE_RTU # set rtu mode
         HTT100.sensors_count += 1
-        print("---------------------------")
-        print("|          INFO           |")
-        print("---------------------------")
-        print("|   Port: " +str(port)+ "         |")
-        print("|   Slave adress: " +str(slave_adress))
-        print("|   Boudrate" +str(baudrate))
-        print("|   Parity: " +str(parity))
-        print("|   Bytesize" +str(bytesize))
-        print("|   Stopbits: " +str(stopbits))
-        print("|   Timeout" +str(timeout))
-
+        print("    ---------------------------")
+        print("    |      SENSOR "+str(HTT100.sensors_count)+"   INFO    |")
+        print("    ---------------------------")
+        print(("  "), ("Port: ").ljust(20), str(port).ljust(40))
+        print(("  "), ("Slave adress: ").ljust(20), str(slave_adress).ljust(40))
+        print(("  "), ("Boudrate: ").ljust(20), str(baudrate).ljust(40))
+        print(("  "), ("Parity: ").ljust(20), str(parity).ljust(40))
+        print(("  "), ("Bytesize: ").ljust(20), str(bytesize).ljust(40))
+        print(("  "), ("Stopbits: ").ljust(20), str(stopbits).ljust(40))
+        print(("  "), ("Timeout: ").ljust(20), str(timeout).ljust(40))
+        print("")
 
     def get_temperature(self):
         self.temperature = self.instrument.read_register(temperature_register, numberOfDecimals=2, functioncode=3, signed=True)
         return self.temperature
 
 sensor_1 = HTT100(port, slave_adress, baudrate, parity, bytesize, stopbits, timeout)
-print(sensor_1.get_temperature())
+for i in range(10):
+    print("temperature = "sensor_1.get_temperature() +" C")
+    delay(1)
