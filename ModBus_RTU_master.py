997638d2e9f9c2620a32c29945ced9d269822542
Zenkin <zenkinartem1997@gmail.com>
2018-06-21 22:42:25 +0300

    add HTT100 class

--- ModBus_RTU_master.py
+++ ModBus_RTU_master.py
@@ -2,29 +2,35 @@
 
 import minimalmodbus
 import serial
-import serial.rs485
+
+baudrate = 9600 # from datasheet
+parity = 'N'
+bytesize = 8 # from datasheet
+stopbits = 1 # from datasheet
+timeout = 0.05
 
 port = '/dev/ttyUSB1' # serial port
 slave_adress = 16 # 10cc
-## Number of the first register 0x0102 16cc or 258 10cc ##
-hex_number = 102
-dec_number = 258
-dec_name_number = dec_number
-register_number = dec_name_number
-number_of_decimals = 2 # temperature value from -4000 to +12000 C
-baudrate = 9600 # from datasheet
-bytesize = 8 # from datasheet
-stopbits = 2 # from datasheet
-timeout = 0.1 # where to get it from?
+temperature_register = 258
 
-def main():
-    print("starting...")
+class HTT100:
 
-    minimalmodbus.BAUDRATE = baudrate
-    minimalmodbus.TIMEOUT = timeout
-    instrument = minimalmodbus.Instrument(port, slave_adress, mode='rtu')
-    instrument.mode = minimalmodbus.MODE_RTU # rtu or ascii mode
-    print(instrument.read_register(register_number, numberOfDecimals=2, functioncode=3, signed=True)) # Registernumber, number of decimals
-    instrument.debug = True     
+    sensors_count = 0
 
-main()
+    def __init__(self, port, slave_adress, baudrate, parity, bytesize, stopbits, baudrate, timeout):
+        minimalmodbus.BAUDRATE = baudrate
+        minimalmodbus.PARITY = parity
+        minimalmodbus.BYTESIZE  = bytesize
+        minimalmodbus.STOPBITS  = stopbits
+        minimalmodbus.TIMEOUT = timeout 
+        self.instrument = minimalmodbus.Instrument(port, slave_adress, mode='rtu')
+        self.instrument.mode = minimalmodbus.MODE_RTU # set rtu mode
+        HTT100.sensors_count += 1
+
+
+    def get_temperature(self):
+        self.temperature = self.instrument.read_register(temperature_register, numberOfDecimals=2, functioncode=3, signed=True)
+        return self.temperature
+
+sensor_1 = HTT100(port, slave_adress, baudrate, parity, bytesize, stopbits, baudrate, timeout)
+print(sensor_1.get_temperature)
