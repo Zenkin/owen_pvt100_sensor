ab3027117ec5ebcbd472dba73bbf0b95835f6977
Zenkin <zenkinartem1997@gmail.com>
2018-06-21 14:59:48 +0300

    commited

--- ModBus_RTU_master.py
+++ ModBus_RTU_master.py
@@ -9,10 +9,10 @@ slave_adress = 16 # 10cc
 ## Number of the first register 0x0102 16cc or 258 10cc ##
 hex_number = 102
 dec_number = 258
-dec_name_number = 1
+dec_name_number = 2
 register_number = dec_name_number
 number_of_decimals = 2 # temperature value from -4000 to +12000 C
-baudrate = 57600 # from datasheet
+baudrate = 9600 # from datasheet
 bytesize = 8 # from datasheet
 stopbits = 1 # from datasheet
 timeout = 0.1 # where to get it from?
@@ -23,8 +23,7 @@ def main():
     minimalmodbus.BAUDRATE = baudrate
     minimalmodbus.TIMEOUT = timeout
     instrument = minimalmodbus.Instrument(port, slave_adress, mode='rtu')
-    instrument.mode = 'rtu' # rtu or ascii mode
-    instrument.serial.rs485_mode = serial.rs485.RS485Settings()
+    instrument.mode = minimalmodbus.MODE_RTU # rtu or ascii mode
     print(instrument.read_register(register_number, numberOfDecimals=2, functioncode=3, signed=True)) # Registernumber, number of decimals
     instrument.debug = True     
 
